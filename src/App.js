3cc537110e75fb3808876d34dfc44b413db4ae11
Andreas Gropp <starmaerker@googlemail.com>
2018-08-21 07:31:05 +0200

    Chapter 1 Playground

--- src/App.js
+++ src/App.js
@@ -2,18 +2,43 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
+
   render() {
-    return (
+    let time = new Date();
+    return (      
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {list.map((item, index) => {
+          return (            
+            <div key={item.objectID}>
+              <p>{index}</p>
+              <p>{item.author}</p>
+              <p>{item.url}</p>
+              <p>{time.toString()}</p>
+            </div>
+          )
+        })}
       </div>
+
     );
   }
 }
