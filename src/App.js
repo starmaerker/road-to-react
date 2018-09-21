0e2a6461a00f41ed3cad9073cf52601a439197ef
Andreas Gropp <starmaerker@googlemail.com>
2018-09-21 13:11:48 +0200

    split components

--- src/App.js
+++ src/App.js
@@ -52,12 +52,39 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <form>
-          <input type="text"
-          value={searchTerm}
-          onChange={this.onSearchChange} />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item => (
+        <Search 
+          value={searchTerm} 
+          onChange={this.onSearchChange}
+        >
+          Search:
+        </Search>
+        <Table 
+          list={list} 
+          pattern={searchTerm} 
+          onDismiss={this.onDismiss} 
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        { children } <input type="text" value={value} onChange={onChange} />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -66,10 +93,7 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
+              <button onClick={() => onDismiss(item.objectID)} type="button">
                 Dismiss
               </button>
             </span>
